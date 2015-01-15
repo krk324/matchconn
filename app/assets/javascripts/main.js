be124304c5dee4891720bdc27fc064c20b30f690
krk324 <krk32488@gmail.com>
2015-01-15 16:51:56 +0900

    Add invitation code

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -4,8 +4,14 @@ function validateCode()
    var code = document.forms["mc-embedded-subscribe-form"]["code"].value;
    if (code === "" || code !== "oxny")
    {
-       alert("Please enter correct email ID")
-       return false;
+        swal({
+          title: "招待コードが違います。",
+          text: "OXNYは完全招待制です。\n 正しい招待コードを入力してください。",
+          imageUrl:"https://s3-ap-northeast-1.amazonaws.com/oxny/LOGO/Flowerflasklogo_black.png"
+        });
+    return false;
    }
-   return( true );
+    return( true );
 }
+
+$('#mc-embedded-subscribe').on('click', validateCode);
