de18fa3a77c32d9dc66e37209522b9cd023b8fcb
krk324 <krk32488@gmail.com>
2015-01-16 17:01:20 +0900

    Update wordings

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -7,7 +7,7 @@ function validateCode()
    {
         swal({
           title: "招待コードが違います。",
-          text: "OXNYは完全招待制です。\n 正しい招待コードを入力してください。",
+          text: "OXNYは完全招待制です。\n 正しい招待コードをご入力頂ます様、お願いします。",
           imageUrl:"https://s3-ap-northeast-1.amazonaws.com/oxny/LOGO/Flowerflasklogo_black.png"
         });
     return false;
