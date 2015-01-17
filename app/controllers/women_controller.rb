bae966e5f7d4051e55f7531300672bd9c2b82432
krk324 <krk32488@gmail.com>
2015-01-17 09:56:48 +0900

    Add gender selection modal

---
+++ app/controllers/women_controller.rb
@@ -0,0 +1,5 @@
+class WomenController < ApplicationController
+  def index
+
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,14 @@
 HackathonStarterRails::Application.routes.draw do
   devise_for :users, :controllers => {
     :registrations => "users/registrations",
-    :omniauth_callbacks => "users/omniauth_callbacks" 
+    :omniauth_callbacks => "users/omniauth_callbacks"
   }
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
   root 'home#index'
+  resources :women, only:[:index]
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
@@ -43,7 +44,7 @@ HackathonStarterRails::Application.routes.draw do
   #       get 'recent', on: :collection
   #     end
   #   end
-  
+
   # Example resource route with concerns:
   #   concern :toggleable do
   #     post 'toggle'
