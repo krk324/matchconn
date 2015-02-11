7184de5d558f0b2131a0cb994d33ce34158fbad1
krk324 <krk32488@gmail.com>
2015-02-12 00:06:31 +0900

    change to tagyu

---
+++ app/assets/javascripts/main.js
@@ -0,0 +1,18 @@
+// function validateCode()
+// {
+
+//    var code = document.getElementById("code").value;
+
+//    if (code === "" || code !== "2015preevent")
+//    {
+//         swal({
+//           title: "招待コードが違います。",
+//           text: "OXNYは完全招待制です。\n 正しい招待コードをご入力頂ます様、お願いします。",
+//           imageUrl:"https://s3-ap-northeast-1.amazonaws.com/oxny/LOGO/Flowerflasklogo_black.png"
+//         });
+//     return false;
+//    }
+//     return( true );
+// }
+
+// $('[id=mc-embedded-subscribe]').on('click', validateCode);
